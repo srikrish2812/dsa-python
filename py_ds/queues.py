924cf0f9eb795799bd20e7aef1679a4bf5026072
abhay <abhay2001sri@gmail.com>
2024-12-24 03:50:46 -0700

    added queues.py

---
+++ py_ds/queues.py
@@ -0,0 +1,14 @@
+from queue import Queue
+
+q = Queue(maxsize=3)
+print(q.qsize())
+q.put('a')
+q.put('b')
+q.put('c')
+print(q.full())
+print(q.qsize())
+print(q.get())
+print(q.get())
+print(q.get())
+#print(q.get_nowait())
+print(q.empty())
