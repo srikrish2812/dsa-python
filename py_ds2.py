892c6644c49a0d2ebfeb802210833eecd245447b
abhay <abhay2001sri@gmail.com>
2024-12-23 20:15:31 -0700

    basic deque operations

--- py_ds2.py
+++ py_ds2.py
@@ -5,12 +5,12 @@ my_deque.append(4)
 my_deque.appendleft(0)
 
 print(my_deque)
+print(my_deque[3]) # 3 :accessing elements is not efficient in deque compared to lists
+print(my_deque.pop()) # 4
+print(my_deque.popleft()) # 0
 
-print(my_deque.pop())
-print(my_deque.popleft())
-
-my_deque.extend([4,5,6,7])
-my_deque.extendleft([0,-1,-2])
+my_deque.extend([4,5,6,7]) # [1, 2, 3, 4, 5, 6, 7]
+my_deque.extendleft([0,-1,-2]) # [-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
 print(my_deque) # deque([-2, -1, 0, 1, 2, 3, 4, 5, 6, 7])
 
 my_deque.rotate(2)
