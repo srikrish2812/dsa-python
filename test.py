abed0feada4de036e9f9fb41950e18ed1e249cc7
abhay <abhay2001sri@gmail.com>
2024-12-23 19:53:15 -0700

    basic list operations

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+eg_list = [10,20,30,40,50]
 
+print(eg_list[::2]) # [10,30,50]
+
+print(eg_list.pop(2)) # removes and returns the element at index 2 in rg_list
+eg_list.insert(2,25) # inserts 25 at index 2
+print(eg_list) # [10,20,25,40,50]
+
+print(eg_list[4:0:-1]) # [50, 40, 25, 20]
+print(eg_list[::-2]) # [50, 25, 10]
