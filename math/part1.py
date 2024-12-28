c8d976e2d3ee3ccf3c2622c0b1da00c90d55ab5f
abhay <abhay2001sri@gmail.com>
2024-12-28 16:30:11 -0700

    adds math basic part1.py

--- math/part1.py
+++ math/part1.py
@@ -15,5 +15,26 @@ count = 0
 while num!=0:
     num = num//10
     count +=1
-print(f"Number of digits in {n} = {count}")
-#
+print(f"Number of digits in {n} = {count}\n")
+
+# Reverse a number
+num = n
+reversed_num = 0
+while num!=0:
+    last_digit = num%10
+    reversed_num = reversed_num*10 + last_digit
+    num = num//10
+print(f"Reverse of number {n} = {reversed_num}\n")
+
+# check if num is palindrome or not
+NUM = 1331
+num = NUM
+rev_num = 0
+while num!=0:
+    last_digit = num%10
+    rev_num = rev_num*10 + last_digit
+    num //=10
+if NUM == rev_num:
+    print(f"{NUM} is a Palindrome\n")
+else:
+    print(f"{NUM} Not a Palindrome\n")
