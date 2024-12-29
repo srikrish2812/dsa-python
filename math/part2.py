9e567ca593db5e62cb6a80a8715785abd25c2404
abhay <abhay2001sri@gmail.com>
2024-12-28 18:08:23 -0700

    adds math basic part2.py

--- math/part2.py
+++ math/part2.py
@@ -11,3 +11,13 @@ if a==0:
     print(b)
 else:
     print(a)
+
+
+# effective gcd using recursion
+a, b=48, 18
+def gcd(a, b):
+    if a == 0:
+        return b
+    print(a,b)
+    return gcd(b % a, a)
+print(gcd(a,b))
