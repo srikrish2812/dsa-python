a5d61ec4f8b1a4487377d27a09eb00e7dbb4a628
abhay <abhay2001sri@gmail.com>
2024-12-31 02:57:50 -0700

    adds rec_gcd

--- recursion/part1.py
+++ recursion/part1.py
@@ -5,3 +5,11 @@ def cal_sum(n=10):
         return n+cal_sum(n-1)
 print(cal_sum())
 print(cal_sum(n=25))
+
+
+def rec_gcd(a=48, b=54):
+    if a==0:
+        return b
+    return rec_gcd(b%a,a)
+
+print(rec_gcd())
