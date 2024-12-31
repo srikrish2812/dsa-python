4d75dd98f9a80a383409b989b705ed669c1849d3
abhay <abhay2001sri@gmail.com>
2024-12-31 15:41:04 -0700

    adds details about recursion and global var

--- recursion/part1.py
+++ recursion/part1.py
@@ -4,12 +4,25 @@
 3. each copy of the function is in a memory stack
 4. If the memory stack is full it is called stack overflow
 """
+# this causes stack overflow since there is no base case/ stop condition to terminate
+# def hello():
+#     print("Hello World")
+#     hello()
+
+# def main():
+#     hello()
+#     return 0
+
+i = 0
 def hello():
-    print("Hello World")
+    global i # tells the function to use the global variable i
+    if i==4:return
+    i+=1
+    print("Hello")
     hello()
 
 def main():
     hello()
-    return 0
 
-main()
+if __name__ == "__main__":
+    main()
