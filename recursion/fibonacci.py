29138e35471d273c337ce5fef20a85addd418c80
abhay <abhay2001sri@gmail.com>
2025-01-01 02:48:55 -0700

    adds prime sum  fibonacci

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -6,3 +6,35 @@ def fib(n):
     return fib(n-1) + fib(n-2)
 
 print(fib(3))
+
+
+# sum of primes till nth fibonacci
+fib_cache = {}
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+def get_fib(n):
+    if n in fib_cache:
+        return fib_cache[n]
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    fib_cache[n] = get_fib(n-1) + get_fib(n-2)
+    return fib_cache[n]
+
+def sum_nthfib(n):
+
+    s = 0
+    for i in range(n+1):
+        ith_fib = get_fib(i)
+        if is_prime(ith_fib):
+            s+=ith_fib
+    return s
+
+print(sum_nthfib(35))
