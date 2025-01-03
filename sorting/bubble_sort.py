9fa4ee85ee598fe7ac9dd475d48a39e774afec2e
abhay <abhay2001sri@gmail.com>
2025-01-02 23:36:32 -0700

    minor changes

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -7,8 +7,8 @@ Bubble Sort:
 
 def bub_sort(arr):
     n = len(arr)
-    for i in range(n-1,0,-1):
-        for j in range(0,i):
+    for i in range(n-1,0,-1): # reduces the range from left
+        for j in range(0,i): # iterates the elements and compares them
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1] = arr[j+1],arr[j]
     return arr
