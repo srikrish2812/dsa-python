fe48c10b44f03f6fa3487410f2030773e681994f
abhay <abhay2001sri@gmail.com>
2025-01-02 23:10:38 -0700

    adds bubble sort

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,16 @@
+"""
+Bubble Sort:
+    1. Push the maximum to the right by swapping adjacent elements
+    2. Reduce the range from the left
+    3. Repeat 1 and 2
+"""
+
+def bub_sort(arr):
+    n = len(arr)
+    for i in range(n-1,0,-1):
+        for j in range(0,i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+print(bub_sort(arr=[2,5,4,8,1]))
