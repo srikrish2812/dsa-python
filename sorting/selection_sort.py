361932292c594948bd45e9f796421290b62bdc3c
abhay <abhay2001sri@gmail.com>
2025-01-02 19:43:17 -0700

    modifies sel_sort function

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -6,10 +6,11 @@ Selection sorting:
 """
 def sel_sort(arr):
     for i in range(len(arr)-1):
+        min_index = i
         for j in range(i+1, len(arr)):
-            if arr[i]>arr[j]:
-                arr[i],arr[j] = arr[j],arr[i]
-
+            if arr[min_index]>arr[j]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
-print(sel_sort(arr=[4,3,2,1]))
+print(sel_sort(arr=[70,40,30,10,50,60]))
