da847b2cf820b4fde77d8df2f2c9173443f10365
abhay <abhay2001sri@gmail.com>
2025-01-04 02:50:25 -0700

    chore: adds merge sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -6,3 +6,45 @@ Merge Sort:
 
 Worst Case TC = O(nlogn)
 """
+
+def merge(arr, left, mid, right):
+    i = left
+    j = mid + 1
+    k = 0
+    n = right - left + 1
+    new_arr = [0] * n
+
+    while i <= mid and j <= right:
+        if arr[i] < arr[j]:
+            new_arr[k] = arr[i]
+            i += 1
+        else:
+            new_arr[k] = arr[j]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        new_arr[k] = arr[i]
+        i += 1
+        k += 1
+
+    while j <= right:
+        new_arr[k] = arr[j]
+        j += 1
+        k += 1
+
+    for c in range(n):
+        arr[left + c] = new_arr[c]
+
+def merge_sort(arr, left, right):
+    if left == right:
+        return
+    mid = (left + right) // 2
+    merge_sort(arr, left, mid)
+    merge_sort(arr, mid + 1, right)
+    merge(arr, left, mid, right)
+
+n = int(input())
+arr = list(map(int, input().split()))
+merge_sort(arr, 0, n - 1)
+print(*arr)
