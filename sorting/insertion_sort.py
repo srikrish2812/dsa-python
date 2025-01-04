431ec9257b2aaefe2cb17cf148845434a6af9c2f
abhay <abhay2001sri@gmail.com>
2025-01-03 19:29:22 -0700

    minor cahnges

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,8 +1,10 @@
 """
 Insertion Sort:
     1. Insert last element in the range in it's correct position
-    2. Start with the second element in the array from left
+    2. Start the range with the second element in the array from left(i=1)
     3. Increase range by 1
+
+In other words it takes the last elemetn in the array and
 
 Worst case TC = O(n^2)
 Average TC = O(n^2)
@@ -12,6 +14,8 @@ Best case TC = O(n) # when array is already sorted
 def insert_sort(arr):
     n = len(arr)
     for i in range(1,n):
+        # this while loop makes sure that j always points to the
+        # last element in the range in the array setting of the outer loop's iteration
         j=i
         while arr[j-1]>arr[j] and j>=1:
             arr[j], arr[j-1] = arr[j-1], arr[j]
