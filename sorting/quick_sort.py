a4e37598f237aec8c8635920d8a6872277ed133f
abhay <abhay2001sri@gmail.com>
2025-01-12 16:58:27 -0700

    adds quick sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,5 +1,9 @@
 """
 Quick Sort:
+    1. Select a pivot. Generally pivot is the arr[0]
+    2. Place pivot in the correct position in the sorted array
+    3. Create a parition at the pivot by placing elements less
+    than pivot to the left and place elements greater than pivot to the right
 
 """
 def quick_sort(arr, left, right):
