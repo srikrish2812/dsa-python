418b84d0e88cdaf9af66af6faea105a3d28b8bdb
abhay <abhay2001sri@gmail.com>
2025-01-12 18:26:54 -0700

    completes quick sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -1,15 +1,40 @@
 """
 Quick Sort:
-    1. Select a pivot. Generally pivot is the arr[0]
+    1. Select a pivot. Pivot is the first element in the range
     2. Place pivot in the correct position in the sorted array
     3. Create a parition at the pivot by placing elements less
-    than pivot to the left and place elements greater than pivot to the right
+    than pivot to the left and place elements greater than pivot
+    to the right
 
 """
-def quick_sort(arr, left, right):
-    ...
+def quick_sort_helper(arr, low, high):
+    i = low
+    j = high
+    while i<j:
+        while arr[i]<=arr[low]: # increment i
+            i+=1
+        while arr[j]>arr[low]: # decrement j
+            j-=1
+        if i<j:
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[low],arr[j] = arr[j],arr[low]
+    return j
+
+def quick_sort(arr, low, high):
+    if low<high:
+        # at index j partition occurs
+        j = quick_sort_helper(arr, low, high) # return the index j
+        quick_sort(arr,low,j-1)
+        quick_sort(arr,j+1, high)
+    else:
+        return
+
 def main():
-    ...
+    arr = [11,17,9,13,16,7,4,10,6]
+    high = len(arr)-1
+    quick_sort(arr=arr,low=0,high=high)
+    print(' '.join(list(map(str,arr))))
+
 
 
 if __name__ =="__main__":
