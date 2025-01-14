8b3ba49b27256ae8953b24358dae24a073c112fb
abhay <abhay2001sri@gmail.com>
2025-01-14 00:07:48 -0700

    updates arrays code

---
+++ arrays/lar_el.py
@@ -0,0 +1,23 @@
+"""
+1. Arrays have the same datatype and contiguous memory
+2. Brute force approach is a very simple solution and is not efficient
+3. Optimal approach is an efficient solution
+
+Find the largest element in the array
+"""
+
+def brute_force(arr):
+    # TC = O(nlogn)
+    arr.sort()
+    return arr[len(arr)-1]
+
+def optimal(arr):
+    # TC = O(n)
+    max_el = arr[0]
+    for el in arr:
+        if el>max_el: max_el=el
+    return max_el
+
+a = [30,20,70,90,15]
+print("Brute Force:",brute_force(a))
+print("Optimal:",optimal(a))
