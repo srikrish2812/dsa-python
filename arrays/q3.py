d75b4a2c56c9935f3da50ed612415cb2511c6aa8
abhay <abhay2001sri@gmail.com>
2025-01-16 15:45:08 -0700

    adds checks if arr is sorted

--- arrays/q3.py
+++ arrays/q3.py
@@ -3,3 +3,15 @@
 2. Remove duplicates
 3. Move zeros to the end
 """
+
+def is_sorted(arr):
+    n = len(arr)
+    for i in range(0,n-1):
+        if arr[i]>arr[i+1]:
+            return False
+    return True
+
+arr_1 = [2,3,3,4,5,11]
+arr_2 = [2,3,4,10,5,12]
+print(f"Check if {arr_1} is sorted: {is_sorted(arr_1)}")
+print(f"Check if {arr_2} is sorted: {is_sorted(arr_2)}")
