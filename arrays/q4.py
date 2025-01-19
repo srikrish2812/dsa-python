c40f77121f09a3ba2587fbe6b5d691de46378208
abhay <abhay2001sri@gmail.com>
2025-01-18 20:03:09 -0700

    adds optimal approach to arrays/q4.py -> move zeros to end

--- arrays/q4.py
+++ arrays/q4.py
@@ -24,3 +24,23 @@ def brute_force(arr):
     return arr
 
 brute_force([0,0,3,0,0,5,0,0,7])
+
+
+def optimal(arr):
+    """
+    Two Pointer approach:
+        1. i will tell us where to place the non-zero element
+        2. j will find the non-zero element
+        3. i=0, j=0(even index 0 can have a non-zero element)
+
+    TC = O(n) -> iterates over the entire array
+    SC = O(n) -> no additional spaces created
+    """
+    i=0
+    for j in range(0,len(arr)):
+        if arr[j]!=0:
+            arr[i], arr[j]=arr[j], arr[i]
+            i+=1
+    print(arr)
+    return i+1 # number of non-zero elements
+optimal(arr=[0,0,3,0,0,4,0,0,7])
