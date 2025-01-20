d577f920087745411c9cb9dc9c5aada940b9e061
abhay <abhay2001sri@gmail.com>
2025-01-20 00:25:04 -0700

    adds linear search code too

--- arrays/q5.py
+++ arrays/q5.py
@@ -17,14 +17,19 @@ print(left_rotate())
 """
 Left rotate the array by k-places
 """
-def kleft_rotate(k=3,arr=[1,2,3,4,5,6,7]):
+def kleft_rotate_brute(k=3,arr=[1,2,3,4,5,6,7]):
     """
     1. Store k-places in temp
     2. shift rest of the elements by k-places
     3. place the elements in temp at the end of array
+
+    TC = O(n+k)
+    SC = O(k)
     """
     temp = []
     n = len(arr)
+    if k>n:
+        k=k%n
     for el in arr[0:k]:
         temp.append(el)
 
@@ -36,4 +41,34 @@ def kleft_rotate(k=3,arr=[1,2,3,4,5,6,7]):
 
     print(f"After shifting {k} places arr = {arr}")
 
-kleft_rotate()
+kleft_rotate_brute(k=10)
+
+
+def kleft_rotate_optimal(k=3,arr=[1,2,3,4,5,6,7]):
+    """
+    1. Reverse the 1st k elements
+    2. Reverse the last n-k elements
+    3. Then reverse the whole array
+
+    TC = O(k) + O(n-k) + O(n) + O(n) = O(n)
+    O(k) -> reversing first k elements
+    O(n-k) -> reversing last n-k elements
+    O(n) -> concatenation of lists
+    O(n) -> reversing the list again
+    SC = O(1)
+    """
+    n = len(arr)
+    arr =arr[k-1::-1] + arr[n-1:k-1:-1]
+    arr = arr[::-1]
+    print(arr)
+kleft_rotate_optimal()
+
+def linear_search(target=6, arr=[12,3,4,5,6,7,77]):
+    """
+    retunr the index of the target element in arr
+    """
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+print(linear_search())
