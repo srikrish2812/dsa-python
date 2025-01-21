42bf939f75467130e1ead9dacc5db72cfdc970dd
abhay <abhay2001sri@gmail.com>
2025-01-21 11:22:31 -0700

    adds code to find missing number using brute force

--- arrays/q8.py
+++ arrays/q8.py
@@ -1,5 +1,6 @@
 """
 1. Maximum consecutive ones
+2. Find missing number
 """
 
 def maxcon_ones(arr=[1,0,1,1,0,1,1,1,1,0]):
@@ -19,3 +20,17 @@ def maxcon_ones(arr=[1,0,1,1,0,1,1,1,1,0]):
     return max
 
 print(maxcon_ones())
+
+
+def find_miss_brute(arr=[5,3,1,6,2],n=6):
+    """
+    TC = O(n^2)
+    SC = O(1)
+    """
+    for i in range(1,n+1):
+        present=0
+        for j in range(0,n-1):
+            if i==arr[j]:
+                present=1
+                break
+        if present==0: return i
