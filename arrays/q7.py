9693dcb5bb8981bbd86999b0a3ca4d16762f3d77
abhay <abhay2001sri@gmail.com>
2025-01-21 10:56:58 -0700

    adds optimal apprach for intersection of 3 sorted arrays

--- arrays/q7.py
+++ arrays/q7.py
@@ -47,3 +47,23 @@ def optimal(a = [2,3,3,3,4,5],b=[3,3,4,4,]):
     return ans
 
 print("Optimal Apprach =", optimal())
+
+
+def get_intersect_three(a=[1,4,6,9],b=[4,6,7,10],c=[4,5,6,8]):
+    ans =[]
+    i,j,k=0,0,0
+    while i<len(a) and j<len(b) and k<len(c):
+        if a[i]==b[j] and b[j]==c[k] and c[k]==a[i]:
+            ans.append(a[i])
+            i+=1
+            j+=1
+            k+=1
+        elif a[i]<b[j]:
+            i+=1
+        elif b[j]<c[k]:
+            j+=1
+        else:
+            k+=1
+    return ans
+
+print("Optimal approach for intersection of 3 arrays",get_intersect_three())
