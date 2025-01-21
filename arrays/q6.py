d265845dea206ceb6fb0994bff73603d5bfce943
abhay <abhay2001sri@gmail.com>
2025-01-21 00:43:10 -0700

    added optimal approach to get union of two sorted arrays

--- arrays/q6.py
+++ arrays/q6.py
@@ -2,7 +2,7 @@
 Union of two arrays:
 """
 
-def brute_force(a=[3,3,4,5,6,10,7,7],b=[4,5,6,7,8,8]):
+def brute_force(a=[3,3,4,5,6,10,7,7],b=[4,5,10,6,7,8,8]):
     """
     m -> len(a)
     n -> len(b)
@@ -24,4 +24,37 @@ def brute_force(a=[3,3,4,5,6,10,7,7],b=[4,5,6,7,8,8]):
         ans.append(el)
     return ans
 
-print(brute_force())
+print("Brute Force Approach",brute_force())
+
+
+def optimal(a = [3,3,4,5,6,7,7,10], b=[4,5,6,7,8,8]):
+    """
+    Optimal solution can be obtained using two pointer approach.
+    For this to work the arrays a and b must be sorted
+    TC = O(m+n)
+    SC = O(m+n)
+    """
+    ans = []
+
+    i,j=0,0
+    while i<len(a) and j<len(b):
+        if a[i]<=b[j]:
+            if not ans or a[i]!=ans[-1]:
+                ans.append(a[i])
+            i+=1
+        else:
+            if not ans or b[j]!=ans[-1]:
+                ans.append(b[j])
+            j+=1
+    while j<len(b):
+        if not ans or b[j]!=ans[-1]:
+            ans.append(b[j])
+        j+=1
+    while i<len(a):
+        if not ans or a[i]!=ans[-1]:
+            ans.append(a[i])
+        i+=1
+
+    return ans
+
+print("Optimal approach", optimal())
