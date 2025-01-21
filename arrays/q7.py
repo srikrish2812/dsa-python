f6ee31c918a33f4cf3df72d100a7b384d1b73d4b
abhay <abhay2001sri@gmail.com>
2025-01-21 10:38:58 -0700

    adds optimal approach for intersection of two sorted arrays

--- arrays/q7.py
+++ arrays/q7.py
@@ -4,8 +4,8 @@ Intersection of two sorted arrays
 
 def brute_force(a=[2,3,3,3,4,4,5],b=[3,3,4,4,]):
     """
-    m -> len(a)
-    n -> len(b)
+    m -> max(len(a),len(b))
+    n -> min(len(a),len(b))
     TC = O(mn)
     SC = O(2n)
     """
@@ -24,3 +24,26 @@ def brute_force(a=[2,3,3,3,4,4,5],b=[3,3,4,4,]):
     return ans
 
 print("Brute Force Approach =", brute_force())
+
+
+def optimal(a = [2,3,3,3,4,5],b=[3,3,4,4,]):
+    """
+    Two pointer approach
+
+    TC = O(m+n)
+    SC = O(min(m,n)) = O(n)
+    """
+    ans = []
+    i,j=0,0
+    while i<len(a) and j<len(b):
+        if a[i]==b[j]:
+            ans.append(a[i])
+            i+=1
+            j+=1
+        elif a[i]<b[j]:
+            i+=1
+        else:
+            j+=1
+    return ans
+
+print("Optimal Apprach =", optimal())
