60a324d43e58b1cfbe13c3f4c90eab32280607a0
abhay <abhay2001sri@gmail.com>
2025-01-27 10:56:43 -0700

    adds better approach for longest subarr with sum k

--- arrays/q10.py
+++ arrays/q10.py
@@ -11,7 +11,7 @@ def brute_appr(arr=ARR, k=K):
     max=0
     n = len(arr)
     for i in range(n):
-        for j in range(n):
+        for j in range(i,n):
             sum=0
             for x in range(i,j+1):
                 sum+=arr[x]
@@ -20,3 +20,21 @@ def brute_appr(arr=ARR, k=K):
                 if length>max: max=length
     return max
 print(brute_appr())
+
+
+def better_appr(arr=ARR, k=K):
+    """
+    TC = O(n^2)
+    SC = O(1)
+    """
+    max = 0
+    n = len(arr)
+    for i in range(n):
+        sum=0
+        for j in range(i,n):
+            sum += arr[j]
+            length = j-i+1
+            if sum==k:
+                if length>max: max=length
+    return max
+print(better_appr())
