6bdf9e88ff734a75fcaf296a31d1690f89c7728f
abhay <abhay2001sri@gmail.com>
2025-01-27 10:49:22 -0700

    adds brute force for longest subarray with sum k

--- arrays/q10.py
+++ arrays/q10.py
@@ -1,3 +1,22 @@
 """
-WOrk in progress
+Longest Subarray with Sum K
 """
+ARR = [1,4,1,3,1,1,2,1,4]
+K = 5
+def brute_appr(arr=ARR, k=K):
+    """
+    TC = O(n^3)
+    SC = O(1)
+    """
+    max=0
+    n = len(arr)
+    for i in range(n):
+        for j in range(n):
+            sum=0
+            for x in range(i,j+1):
+                sum+=arr[x]
+            if sum==k:
+                length=j-i+1
+                if length>max: max=length
+    return max
+print(brute_appr())
