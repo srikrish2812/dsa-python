d3c12a91c1cb6518286deab579aff32299309044
abhay <abhay2001sri@gmail.com>
2025-01-29 11:08:43 -0700

    adds binary search code

---
+++ binary_search/q1.py
@@ -0,0 +1,28 @@
+"""
+Binary Search:
+    1. Initialize low = 0 and high = len(arr)-1
+    2. Calculate mid = (low+high)//2
+    3. Check if target == arr[mid]
+    4. If target<arr[mid] then high=mid and low=low
+    5. If target>arr[mid] then low=mid and high=high
+    6. Repeat this until target==arr[mid]
+
+TC = O(logn)
+SC = O(1)
+"""
+ARR = [2,3,5,6,8,9,12,15]
+def binary_search(arr=ARR,target=12):
+    n = len(arr)
+    low = 0
+    high = n-1
+    while low<=high:
+        mid= (low+high)//2
+        if target==arr[mid]:
+            return mid
+        elif target>arr[mid]:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+
+print(binary_search())
