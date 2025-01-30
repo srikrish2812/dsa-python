e94dd183b4c1d75267e5bcfb1463b3c1f79265e7
abhay <abhay2001sri@gmail.com>
2025-01-30 11:16:12 -0700

    adds code for upper bound problem

--- binary_search/low_up_bound.py
+++ binary_search/low_up_bound.py
@@ -4,7 +4,7 @@ in arr are less than x then return len(arr)
 2. Upper Bound
 """
 ARR = [2,3,5,5,6,7,7,7,10,12]
-X = 8
+X = 7
 def lower_bound(arr=ARR,x=X):
     """
     TC = O(logn)
@@ -22,4 +22,25 @@ def lower_bound(arr=ARR,x=X):
         elif arr[mid]<x:
             low=mid+1
     return ans
-print(lower_bound())
+print("Lower Bound =",lower_bound())
+
+
+def upper_bound(arr=ARR,x=X):
+    """
+    Upper bound is defined as the index of the initial value
+    in the array that surpasses 'x'. If no such element exists
+    then return size of the array
+    """
+    n = len(arr)
+    low=0
+    high=n-1
+    ans = n
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]>x:
+            ans=mid
+            high=mid-1
+        elif arr[mid]<=x:
+            low=mid+1
+    return ans
+print(f"Upper Bound = {upper_bound()}")
