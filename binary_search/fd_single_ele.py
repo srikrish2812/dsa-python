b07c7845ec9d2ecd4fb8b6a9321b479749332041
abhay <abhay2001sri@gmail.com>
2025-02-08 12:07:53 -0700

    adds optimal approach

--- binary_search/fd_single_ele.py
+++ binary_search/fd_single_ele.py
@@ -3,6 +3,10 @@ Find the element that occurs just once in the  sorted array
 """
 ARR = [2,2,4,4,6,6,7,9,9]
 def brute_appr(arr=ARR):
+    """
+    TC = O(n)
+    SC = O(1)
+    """
     n = len(arr)
     if n==1: return arr[0]
     if arr[0]!=arr[1]: return arr[0]
@@ -14,3 +18,31 @@ def brute_appr(arr=ARR):
     return -1
 
 print(brute_appr())
+
+
+def optimal_appr(arr=ARR):
+
+    """
+    1. Before the single element the indices of the pairs are (even,odd)
+    2. After the single element the indices of the pairs are (odd, even)
+
+    """
+    n = len(arr)
+    if n==1: return arr[0]
+    if arr[0]!=arr[1]: return arr[0]
+    if arr[n-2]!=arr[n-1]: return arr[n-1]
+
+    low = 1
+    high = n-2
+    while low<=high:
+        mid = low + (high-low)//2
+
+        if arr[mid]!=arr[mid-1] and arr[mid]!= arr[mid+1]:
+            return arr[mid]
+        if (mid%2==1 and arr[mid]==arr[mid-1]) or (mid%2==0 and arr[mid]==arr[mid+1]):
+            low= mid+1
+        else:
+            high=mid-1
+    return -1
+
+print('Using Optimal Approach:',optimal_appr())
