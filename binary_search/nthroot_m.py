3dee29bc5625c6f2f016a6d6a8e659df260723a8
abhay <abhay2001sri@gmail.com>
2025-02-13 00:44:45 -0700

    adds optimal approach

--- binary_search/nthroot_m.py
+++ binary_search/nthroot_m.py
@@ -3,11 +3,44 @@
 Find the nth root of an integer
 """
 
+def power(k,n):
+    ans=k
+    for _ in range(1,n):
+        ans*=k
+    return ans
+
 def brute_force(n,m):
-    
+    """
+    The power function will run for n times and the
+    below for loop runs k-times where k**n==m => k = m**(1/n)
+
+    TC = O(n*m^(1/n))
+    SC = O(1)
+    """
     for i in range(m):
-        if i**n==m:
+        if power(i,n)==m:
             return i
-    return -1
+        elif power(i,n)>m:
+            return -1
 
 print(f"Using brute force: {brute_force(3,64)}")
+
+
+def opt_appr(n,m):
+    """
+    TC = O(n*logm)
+    SC = O(1)
+    """
+    low =1
+    high = m
+    while low<=high:
+        mid = low + (high-low)//2
+        if power(mid,n)==m:
+            return mid
+        elif power(mid,n)>m:
+            high=mid-1
+        else:
+            low=mid+1
+    return -1
+
+print(f"Using Optimal Approach: {opt_appr(3,62)}")
