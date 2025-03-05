ebaaffdb4e7f2409dbc9179c453099f49c5e289b
abhay <abhay2001sri@gmail.com>
2025-03-05 11:44:44 -0700

    adds code to count the number of subsequences

---
+++ adv_recursion/count_subseq_sumk.py
@@ -0,0 +1,22 @@
+"""
+Count the number of subsequences with sum==k
+"""
+
+def count_subseq(i,s,arr,n,k):
+    if i>=n:
+        if s==k:
+            return 1
+        return 0
+
+    # pick
+    s+=arr[i]
+    left = count_subseq(i+1,s,arr,n,k)
+
+    # not pick
+    s-=arr[i]
+    right = count_subseq(i+1,s,arr,n,k)
+
+    return left + right
+
+
+print("Count of subsequences with sum=k is", count_subseq(0,0,[3,4,7],3,7))
