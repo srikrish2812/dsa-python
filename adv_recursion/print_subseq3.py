dbae36cfe26e6cefa59e932589a5e42b8bf13397
abhay <abhay2001sri@gmail.com>
2025-03-05 11:14:18 -0700

    adds alternate implementation

--- adv_recursion/print_subseq3.py
+++ adv_recursion/print_subseq3.py
@@ -5,13 +5,13 @@ Print only 1 subsequence such that sum==k
 def print_subsequence(i,v,s,arr,n,k, is_printed):
     """
     Using a mutable object for is_printed:
-    Instead of passing a boolean (is_printed), we pass a list [False]. 
-    Lists are mutable, so changes to is_printed[0] in one recursive 
+    Instead of passing a boolean (is_printed), we pass a list [False].
+    Lists are mutable, so changes to is_printed[0] in one recursive
     call are reflected in all other calls.This ensures that once a valid
     subsequence is printed, no further subsequences are printed.
-    
+
     Stopping further recursion:
-    
+
     After printing a valid subsequence, we set is_printed[0] = True.
     This prevents the function from printing any other subsequences.
     """
@@ -30,6 +30,26 @@ def print_subsequence(i,v,s,arr,n,k, is_printed):
     print_subsequence(i+1, v,s,arr,n,k,is_printed)
 
 
+def alt_implementation(i,v,s,arr,n,k):
+    # base case
+    if i>=n:
+        if s==k:
+            print(*v)
+            return True
+        return False
+
+    # pick
+    v.append(arr[i])
+    s+=arr[i]
+    if alt_implementation(i+1,v,s,arr,n,k): return True
+
+    # not pick
+    v.pop()
+    s-=arr[i]
+    if alt_implementation(i+1,v,s,arr,n,k): return True
+
+    return False
+
 if __name__=="__main__":
    ARR = [4,9,5]
    K=9
@@ -37,3 +57,4 @@ if __name__=="__main__":
    i=0
    s=0
    print_subsequence(i,v,s,ARR,len(ARR),K,[False])
+   alt_implementation(i,v,s,ARR,len(ARR),K)
